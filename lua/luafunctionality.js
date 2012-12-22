a2146dd724894f20001a79487e064090397ae45a
boxmein <johannes12321@windowslive.com>
2012-12-22 03:08:01 +0200

    Moved lua-generator related files to lua/

---
+++ lua/luafunctionality.js
@@ -0,0 +1,12 @@
+var addPropertyLine = function () {
+    'use strict';
+    // Adds another property-setting line
+	var g = document.getElementById("propertyList");
+	g.innerHTML += "\nelements.property(el, \"<b>[Property]</b>\", \"<b>[Value]</b>\") ";
+	g.appendChild(g);
+};
+
+var toggleElement = function (elementid) {
+    var d = document.getElementById(elementid);
+    d.style.visibility = d.style.visibility=="hidden" ? "visible" : "hidden";
+};
