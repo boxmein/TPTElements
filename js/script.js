ffaf1a64b5bce8b3e60a31c1d90e0ffe95b9fff5
boxmein <johannes12321@windowslive.com>
2013-06-07 17:07:44 +0300

    Added comments, block => inline fix

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-"use strict";
 
 var toggleComments = function () {
     // Hide or show comments
 	var comments = document.getElementsByTagName("i");
+	// <i></i> is now a designated comment
 	if(comments[0].style.display != "none") {
 		for(var i=0; i<comments.length; i++) {
 			comments[i].style.display = "none";
@@ -10,29 +10,31 @@ var toggleComments = function () {
 	}
 	else {
 		for(var i=0; i<comments.length; i++) {
-			comments[i].style.display = "block";
+			comments[i].style.display = "inline";
 		}
 	}
     
 };
 var getCode = function () {
+	// get what has been written, also convert it to base64
 	return btoa(document.getElementById("fullcode").textContent);
 }
 var initAll = function () {
-    // Make content boxes editable
+  // Make <b>'s editable
 	var editables = document.getElementsByTagName("b");
 	for(var i=0; i<editables.length; i++) {
 		editables[i].setAttribute("contenteditable", "true");
 	}
 };
 var clearCode = function () {
-    // Clear code boxes below
+  // Clear the source code boxes with their example code
 	var codes = document.getElementsByClassName("code");
 	for(var i=0; i<codes.length; i++) {
 		codes[i].innerHTML = "[placeholder]";
 	}
 };
 var chAllToName = function() {
+	// Change all of the <b></b> s' contents to BCOL (where it used to be BCOL)
 	var name = document.getElementById("elname").innerHTML;
 	var editables = document.getElementsByTagName("b");
 	for(var i=0; i<editables.length; i++) {
@@ -41,6 +43,8 @@ var chAllToName = function() {
 	}
 };
 var toggleUnoComments = function() {
+	// uno = un-obstructive
+	// those comments are meant to not stab you in the eye
 	var comments = document.getElementsByTagName("i");
     if(comments[0].className != "uno") {
         for(var i=0; i<comments.length; i++) {
@@ -55,6 +59,10 @@ var toggleUnoComments = function() {
     
 };
 var save = function() {
-	var content = getCode();
-	window.open("data:text/plain;base64," + getCode())
+  // manual save, generates data and opens a new window with it. 
+	window.open("data:text/plain;base64," + getCode());
 }
+
+
+
+toggleUnoComments(); 
