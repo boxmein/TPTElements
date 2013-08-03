f40fa81583b70e3ba2977fc789448b509c944daa
boxmein <johannes12321@windowslive.com>
2013-08-03 17:40:38 +0300

    Added Lua and reference view functions

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,11 @@
 exports.index = function(req, res){
   res.render('index', { c: "//" });
 };
+
+exports.lua = function(req, res){
+  res.render('lua', { c: "--" });
+};
+
+exports.reference = function (req, res) {
+
+};
