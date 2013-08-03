52b18de710d42f8f49cd0afcf3a256c25131c624
boxmein <johannes12321@windowslive.com>
2013-08-03 17:35:09 +0300

    Special save function for Lua

--- public/javascripts/aside-lua.js
+++ public/javascripts/aside-lua.js
@@ -1,10 +1,30 @@
-var addPropertyLine = function() {
-  $("#propertyList").html($("#propertyList").html()+'\n<span class="propline">elements.property(el, <b contenteditable="true">"[Property]"</b>, <b contenteditable="true">"[Value]"</b>)</span>');
-}
-var toggleElement = function(id) {
-	// Utility function for the functionality/graphics function and the cloning line.
-	$("#"+id+":visible").hide("fast"); 
-  $("#"+id+":hidden").show("fast"); 
-}
+
+$("#addprop").click(function() { 
+  $("#propertyList").append($('<li class="propline"><span onclick="$(this).parent().remove()" class="close">[X]</span> elements.property(el, "<b>Name</b>", "<b>AWSM</b>")</li>').find('b').attr('contentEditable', 'true').end());
+});
+
+$("#tclone").click(function() {$("#cloneToggle").toggle('fast');});
+$("#tgraphics").click(function() {$("#graphicsFunc").toggle('fast');});
+$("#tupdate").click(function() {$("#updateFunc").toggle('fast');});
+$("#tmodloader").click(function() {$("#cracker64").toggle('fast');});
+
+getCode = function () {
+  // without this you can only save once per refresh
+  $("#codetemp").remove();
+  var g; 
+  $(document.body).append((g = $('<div id="codetemp" style="display: none">'+$('#code').html()+'</div>')));
+
+  // remove comments
+  g.find("i").remove(); 
+
+  // special togglable elements
+  if ($("#graphicsFunc").is(":hidden"))
+    g.find("#graphicsFunc").remove(); 
+  if ($("#updateFunc").is(":hidden"))
+    g.find("#updateFunc").remove(); 
+  if ($("#cracker64").is(":hidden"))
+    g.find("#cracker64").remove(); 
 
 
+  return btoa(g.text().replace(/\[X\]/g, "\n"));
+};
